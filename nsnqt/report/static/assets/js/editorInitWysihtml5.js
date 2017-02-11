6a12bed2c5063f0f03d99170cf5c529795dbc442
Xu <jian-stuart.xu@nokia.com>
2017-02-12 00:04:51 +0800

    fix bug

---
+++ nsnqt/report/static/assets/js/editorInitWysihtml5.js
@@ -0,0 +1,8 @@
+﻿function formWysiwyg() {
+    "use strict";
+
+    /*----------- BEGIN wysihtml5 CODE -------------------------*/
+    $('#wysihtml5').wysihtml5();
+    /*----------- END wysihtml5 CODE -------------------------*/
+
+}
